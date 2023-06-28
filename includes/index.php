e1012347f41c30d26e234cf4f8abe4080bfdeea4
SandroKE <koomealessandro@gmail.com>
2023-06-29 01:54:28 +0300

    updated Fectch method

--- includes/index.php
+++ includes/index.php
@@ -2,3 +2,20 @@
 
 require 'config.php';
 $sandro = getConn($host, $db_name, $user, $password); 
+$conn = getConn($host, $db_name, $user, $password);
+
+if ($sandro) {
+    try {
+        // Prepare and execute the SQL query
+        $stmt = $conn->prepare("SELECT title FROM post");
+        $stmt->execute();
+
+        // Fetch the title from the result
+        $title = $stmt->fetchColumn();
+
+        // Output the fetched title
+        echo "Fetched Title: " . $title;
+    } catch (PDOException $e) {
+        echo "Query failed: " . $e->getMessage();
+    }
+}
