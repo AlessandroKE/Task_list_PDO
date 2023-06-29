b88b838b6aafc4cae4fd6135da92447b898a4d15
SandroKE <koomealessandro@gmail.com>
2023-06-29 05:28:07 +0300

    updated index.php

--- index.php
+++ index.php
@@ -7,14 +7,20 @@ $sandro = getConn($host, $db_name, $user, $password);
 if ($sandro) {
     try {
         // Prepare and execute the SQL query
-        $stmt = $sandro->prepare("SELECT title FROM post");
+        $stmt = $sandro->prepare("SELECT * FROM post");
         $stmt->execute();
 
         // Fetch the title from the result
-        $title = $stmt->fetchColumn();
+        //$title = $stmt->fetchColumn();
+        //Fetch all titles from the result using fetchAll()
+        $titles = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         // Output the fetched title
-        echo "Fetched Title: " . $title;
+        // using the for each loop to collect title in a  database
+        foreach($titles as $title){
+            print_r ($title);
+        //echo "</br> Fetched Title: $title </br>";
+        }
     } catch (PDOException $e) {
         echo "Query failed: " . $e->getMessage();
     }
