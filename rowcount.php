f9dfa5b9231f849ff85e7ca5be2735cf2d75ab1a
SandroKE <koomealessandro@gmail.com>
2023-06-30 21:51:55 +0300

    initial commit

---
+++ rowcount.php
@@ -0,0 +1,23 @@
+<?php
+
+require ('includes/config.php');
+$conn = getConn($host, $db_name, $user, $password);
+
+$stmt = $conn->prepare("SELECT * FROM post");
+       // $stmt->execute();
+ 
+if($stmt->execute()){
+    $stmt ->rowCount(); 
+
+    //print_r($stmt->rowCount());
+
+    if($stmt->rowCount() > 0){
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            echo $row ['body'] . "<br>";
+    }
+}
+    else{
+
+    }
+}
+
